33ba90ba98ed277378ebdbd4c174d48c6a5786d5
AlexGol005 <pesok1985>
2021-12-15 21:44:31 +0300

    15.12/2

--- main.py
+++ main.py
@@ -132,7 +132,7 @@ def main():
     вызывает генератор списка словарей книг  100 раз и записывает результат в json файл output1.json
     :return: список словарей книг, заполненный json файл
     """
-    return  list_gen(50)
+    return  list_gen(100)
 
 
 if __name__ == "__main__":
